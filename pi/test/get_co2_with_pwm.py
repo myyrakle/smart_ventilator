366be3e43c27f3ab42a970f3b336e37f09e67cfb
myyrakle <sssang97@naver.com>
2019-04-18 19:41:19 +0900

    Create get_co2_with_pwm.py

---
+++ pi/test/get_co2_with_pwm.py
@@ -0,0 +1,49 @@
+import time
+import RPi.GPIO as GPIO
+
+PIN_PWM = 18
+
+def get_co2_ppm():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN_PWM, GPIO.IN)
+    
+    time.sleep(0.2)
+    
+    
+    while GPIO.input(PIN_PWM) == 1:
+        last_high = time.time()
+        
+    while GPIO.input(PIN_PWM) == 0:
+        last_low = time.time()
+        
+    while GPIO.input(PIN_PWM) == 1:
+        last_high = time.time()
+        
+
+    span_high = (last_high - last_low) * 1000
+    #print("span_low : " + str(span_high))
+    
+    
+    
+    while GPIO.input(PIN_PWM) == 0:
+        last_low = time.time()
+        
+    while GPIO.input(PIN_PWM) == 1:
+        last_high = time.time()
+        
+    while GPIO.input(PIN_PWM) == 0:
+        last_low = time.time()
+        
+
+    span_low = (last_low - last_high) * 1000
+    #print("span_low : " + str(span_low))
+
+
+    #print("Cycle : " + str(span_high + span_low))
+
+    CO2 = 5000 * ( span_high - 2 ) / ( span_high + span_low - 4 )
+    AQ = "%.0f" % CO2
+    GPIO.cleanup()
+    return AQ
+
